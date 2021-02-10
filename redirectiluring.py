e4f7f9491687f211427f9b22df88ad5922915030
narken <narken.spam@gmail.com>
2021-02-10 18:53:30 +0100

    Rework

--- redirectiluring.py
+++ redirectiluring.py
@@ -6,16 +6,23 @@ conf_file = open('test.json')
 server_data = json.load(conf_file)
 
 class skurkHandler(BaseHTTPRequestHandler):
-    def _set_response(self, handler_rules):
+
+    def _get_path(self):
+        s = self.path
+        sep = [s.find('?'), s.find('&'), s.find('#'), len(s)]
+        sep = min([x for x in sep if x != -1])
+        print(sep)
+        self.path = self.path[:sep]
+
+    def _set_response(self, method):
+        handler_rules = server_data[method]
+        self._get_path()
         if not handler_rules:
-            self._set_response(server_data["default"])
+            self._set_response("default")
         else:
             if not self.path in handler_rules:
-                if not "default" in handler_rules:
-                    self.path = "default"
-                else:
-                    handler_rules = server_data["default"]
-                    self.path = "default"
+                print("Path not in config - setting to /")
+                self.path = "/"
 
             self.send_response(int(handler_rules[self.path]["code"]))
             for header in handler_rules[self.path]["headers"]:
@@ -30,31 +37,31 @@ class skurkHandler(BaseHTTPRequestHandler):
                 self.wfile.write(handler_rules[self.path]["body"].encode('utf-8'))
         
     def do_GET(self):
-        self._set_response(server_data["GET"])
+        self._set_response("GET")
 
     def do_POST(self):
-        self._set_response(server_data["POST"])
+        self._set_response("POST")
 
     def do_PUT(self):
-        self._set_response(server_data["PUT"])
+        self._set_response("PUT")
 
     def do_HEAD(self):
-        self._set_response(server_data["HEAD"])
+        self._set_response("HEAD")
 
     def do_OPTIONS(self):
-        self._set_response(server_data["OPTIONS"])
+        self._set_response("OPTIONS")
 
     def do_PATCH(self):
-        self._set_response(server_data["PATCH"])
+        self._set_response("PATCH")
 
     def do_DELETE(self):
-        self._set_response(server_data["DELETE"])
+        self._set_response("DELETE")
 
     def do_CONNECT(self):
-        self._set_response(server_data["CONNECT"])
+        self._set_response("CONNECT")
 
     def do_TRACE(self):
-        self._set_response(server_data["TRACE"])
+        self._set_response("TRACE")
 
 
 def run(server_class=HTTPServer, handler_class=skurkHandler, port=8000):
